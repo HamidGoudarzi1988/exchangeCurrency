a46a173c52020d8d72446a6a357ecc15c7f58135
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-08-19 19:53:34 -0400

    Updates

---
+++ precache-manifest.eacfccd7333b5df833fd92abe1a625b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57795ef7d36e3aeeebabb5c98fe2cf0",
+    "url": "/exchangeCurrency/index.html"
+  },
+  {
+    "revision": "f3b7e44f030b183d51a2",
+    "url": "/exchangeCurrency/static/css/main.550d594e.chunk.css"
+  },
+  {
+    "revision": "9712cf3d1fe8ccb40c82",
+    "url": "/exchangeCurrency/static/js/2.991d0d0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchangeCurrency/static/js/2.991d0d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b7e44f030b183d51a2",
+    "url": "/exchangeCurrency/static/js/main.f180c342.chunk.js"
+  },
+  {
+    "revision": "7f84e490c414393951be",
+    "url": "/exchangeCurrency/static/js/runtime-main.50e7cb15.js"
+  }
+]);
